03a91d680b9f0e55d86bc8d8d682b10be5a85c02
waqarz <waqar.zia@gmail.com>
2015-06-16 13:16:01 +0200

    Applied liscence to files. RD examples still missing from Git? Added

--- webfe/analyticstracking.php
+++ webfe/analyticstracking.php
@@ -1,5 +1,19 @@
 <?php
 
+/*This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
--- webfe/process.php
+++ webfe/process.php
@@ -1,4 +1,19 @@
 <?php
+
+/*This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 ini_set('memory_limit','-1');//remove memory limit
 error_reporting(E_ERROR | E_PARSE);
 include 'featurlist.php'; 
