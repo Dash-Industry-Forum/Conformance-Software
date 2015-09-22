4f08f5ea41175247d2177cd5268e554fc1d0ce50
li <li@test05.(none)>
2015-09-22 11:52:25 +0200

    merge the changes from Niteesh

---
+++ webfe/save_xml.php
@@ -0,0 +1,21 @@
+
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$filename=$_REQUEST['filename'];
+
+//filter_input(INPUT_REQUEST, 'dirid');
+  $xml = $_POST['content'];
+  //filter_input(INPUT_POST, 'file_contents');
+  $file = fopen($filename,"wb");
+  fwrite($file, $xml);
+  
+  fclose($file);
+  
+
+?> 
