950d5cd1ff9e4add4d8639a82b7eab1f20129298
Yi Li <yili.buaa@gmail.com>
2016-01-21 14:18:03 +0100

    Fix the problem that the error log is not show properly for DTS test vectors

--- webfe/give.php
+++ webfe/give.php
@@ -1,25 +1,68 @@
 <?php
 
-/*This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+/* This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-if(isset($_POST['val'])){
-$filename=$_POST['val'];
+if (isset($_POST['val'])) {
+    $filename = $_POST['val'];
 //$string = file_get_contents ('./temp/'.$filename.'.txt',true);
-$stringarr = file('./temp/'.$filename.'.txt',FILE_IGNORE_NEW_LINES);
-$string=json_encode($stringarr);
+    $stringarr = file('./temp/' . $filename . '.txt', FILE_IGNORE_NEW_LINES);
+    $string = safe_json_encode($stringarr);
 
-echo $string;
+//    if ($string == false)
+//    {        
+//        $input = iconv('UTF-8', 'UTF-8//IGNORE', utf8_encode($stringarr));
+//        $string = json_encode($input);
+//    }
+    echo $string;
 }
+
+function safe_json_encode($value){
+    if (version_compare(PHP_VERSION, '5.4.0') >= 0) {
+        $encoded = json_encode($value, JSON_PRETTY_PRINT);
+    } else {
+        $encoded = json_encode($value);
+    }
+    switch (json_last_error()) {
+        case JSON_ERROR_NONE:
+            return $encoded;
+        case JSON_ERROR_DEPTH:
+            return 'Maximum stack depth exceeded'; // or trigger_error() or throw new Exception()
+        case JSON_ERROR_STATE_MISMATCH:
+            return 'Underflow or the modes mismatch'; // or trigger_error() or throw new Exception()
+        case JSON_ERROR_CTRL_CHAR:
+            return 'Unexpected control character found';
+        case JSON_ERROR_SYNTAX:
+            return 'Syntax error, malformed JSON'; // or trigger_error() or throw new Exception()
+        case JSON_ERROR_UTF8:
+            $clean = utf8ize($value);
+            return safe_json_encode($clean);
+        default:
+            return 'Unknown error'; // or trigger_error() or throw new Exception()
+
+    }
+}
+
+function utf8ize($mixed) {
+    if (is_array($mixed)) {
+        foreach ($mixed as $key => $value) {
+            $mixed[$key] = utf8ize($value);
+        }
+    } else if (is_string ($mixed)) {
+        return utf8_encode($mixed);
+    }
+    return $mixed;
+}
+
 ?>
