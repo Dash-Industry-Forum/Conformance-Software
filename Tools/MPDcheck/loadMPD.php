ef6ee5d79523f9d6a0810d5380e0f6a24c613857
Yi Li <yili.buaa@gmail.com>
2016-02-25 14:28:59 +0100

    Adapt to more http protocols

--- Tools/MPDcheck/loadMPD.php
+++ Tools/MPDcheck/loadMPD.php
@@ -1,16 +1,11 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
+//header('Access-Control-Allow-Origin: *');
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 $url = $_REQUEST['url'];
 
 $file_headers = @get_headers($url);
-if ($file_headers[0] == 'HTTP/1.1 404 Not Found') {
+if ($file_headers[0] == 'HTTP/1.0 404 Not Found' || $file_headers[0] == 'HTTP/1.1 404 Not Found' || $file_headers[0] == 'HTTP/2 404 Not Found') {
     $exists = false;
 } else {
     $exists = true;
@@ -24,8 +19,6 @@ if($exists)
 else {
     echo 'error';
 }
-// if ($response_xml_data) {
-//     $data = simplexml_load_string($response_xml_data);
-// }
+
 ?>
 
