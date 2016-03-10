3e599f646d89e809389e92ee3e545341eb0c18e1
Yi Li <yili.buaa@gmail.com>
2016-03-10 20:03:36 +0100

    Cleaned up the code. Added a few features: 1. show cross-representation validation error log when there's an error. 2. add to progress.xml the timestamp in seconds w.r.t. UTC time. Clean the temp folders after 5 mins after test completed or 30 mins after test started 3. show the broken URL lists when there are some segments missing during downloading

--- webfe/process.php
+++ webfe/process.php
@@ -1,22 +1,22 @@
 <?php
 
-/*This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+/* This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-ini_set('memory_limit','-1');//remove memory limit
+ini_set('memory_limit', '-1'); //remove memory limit
 error_reporting(E_ERROR | E_PARSE);
-include 'featurlist.php'; 
+include 'featurlist.php';
 //include 'globalvariables.php';
 include 'crossvalidation.php';
 include 'mpdvalidation.php';
@@ -26,80 +26,80 @@ include 'datadownload.php';
 include 'assemble.php';
 include 'schematronIssuesAnalyzer.php';
 
-set_time_limit(0);// php run without time limit
+set_time_limit(0); // php run without time limit
 ini_set("log_errors", 1);
 ini_set("error_log", "myphp-error.log");
 
 $session_id = json_decode($_POST['sessionid']); // parse recieved data
-session_name($session_id); 			
-session_start();// initiate session for connected client
-error_log( "session_start:".session_name() );
+session_name($session_id);
+session_start(); // initiate session for connected client
+error_log("session_start:" . session_name());
 
-$adaptsetdepth=array();// array for Baseurl 
-			$depth = array();//array contains all relative URLs exist in all mpd levels 
-	        $locate ;  // location of session folder on server
-			$foldername; // floder name for the session
-            $Adapt_urlbase = 0; // Baseurl in adaptationset
-	        $id = array();  //mpd id
-            $codecs = array(); 
-			$width = array (); 
-			$height = array ();
-			$period_baseurl=array();// all baseURLs included in given period
-			$scanType = array(); 
-			$frameRate = array();
-			$sar=array();
-			$bandwidth=array();
-            $Adaptationset=array();//array of all attributes in single adapatationset
-			$Adapt_arr = array();//array of all adaptationsets within 1 period
-			$Period_arr= array(); // array of all periods 
-			$init_flag; // flag decide if this is the first connection attempt
-            $repnolist = array(); // list of number of representation
-            $period_url = array(); // array contains location of all segments within period
-			$perioddepth=array(); //array with all relative baseurls up to period level
-			$type = "";
-            $minBufferTime = "";
-            $profiles = "";
-            $mediaPresentationDuration = "";
-			$count1=0; // Count number of adaptationsets processed
-			$count2=0;;//count number of presentations proceessed
-			if(isset($_POST['urlcode'])){// if client initiate first connection
+$adaptsetdepth = array(); // array for Baseurl 
+$depth = array(); //array contains all relative URLs exist in all mpd levels 
+$locate;  // location of session folder on server
+$foldername; // floder name for the session
+$Adapt_urlbase = 0; // Baseurl in adaptationset
+$id = array();  //mpd id
+$codecs = array();
+$width = array();
+$height = array();
+$period_baseurl = array(); // all baseURLs included in given period
+$scanType = array();
+$frameRate = array();
+$sar = array();
+$bandwidth = array();
+$Adaptationset = array(); //array of all attributes in single adapatationset
+$Adapt_arr = array(); //array of all adaptationsets within 1 period
+$Period_arr = array(); // array of all periods 
+$init_flag; // flag decide if this is the first connection attempt
+$repnolist = array(); // list of number of representation
+$period_url = array(); // array contains location of all segments within period
+$perioddepth = array(); //array with all relative baseurls up to period level
+$type = "";
+$minBufferTime = "";
+$profiles = "";
+$mediaPresentationDuration = "";
+$count1 = 0; // Count number of adaptationsets processed
+$count2 = 0; //count number of presentations proceessed
 
-$url_array = json_decode($_POST['urlcode']); // parse recieved data
-$url = $url_array[0];// get mpd url from HTTP request
-$_SESSION['url']=$url;// save mpd url to session variable
-unset($_SESSION['period_url']); // reset session variable 'period_url' in order to remove any old segment url from previous sessions
-unset($_SESSION['init_flag']);// reset for flag indicating first connection attempt
+if (isset($_POST['urlcode'])) {// if client initiate first connection
+    $url_array = json_decode($_POST['urlcode']); // parse recieved data
+    $url = $url_array[0]; // get mpd url from HTTP request
+    $_SESSION['url'] = $url; // save mpd url to session variable
+    unset($_SESSION['period_url']); // reset session variable 'period_url' in order to remove any old segment url from previous sessions
+    unset($_SESSION['init_flag']); // reset for flag indicating first connection attempt
 }
-			if(isset($_SESSION['locate'])) //get location from session variable if it is not secont  attempt to access server by same session
-			$locate = $_SESSION['locate'];
-			$Timeoffset;
- if(isset($_SESSION['count1']))//get Adaptationset counter in access
- $count1 =$_SESSION['count1'];
- 
- if(isset($_SESSION['foldername']))//get folder name from session
- $foldername=$_SESSION['foldername'];
- 
-  if(isset($_SESSION['count2']))//get presentation counter
- $count2 =$_SESSION['count2'];
+if (isset($_SESSION['locate'])) //get location from session variable if it is not secont  attempt to access server by same session
+    $locate = $_SESSION['locate'];
 
- if (isset($_SESSION['url']))//get mpd url from session variable
- $url=$_SESSION['url'];
- 
+if (isset($_SESSION['count1']))//get Adaptationset counter in access
+    $count1 = $_SESSION['count1'];
+
+if (isset($_SESSION['foldername']))//get folder name from session
+    $foldername = $_SESSION['foldername'];
+
+if (isset($_SESSION['count2']))//get presentation counter
+    $count2 = $_SESSION['count2'];
+
+if (isset($_SESSION['url']))//get mpd url from session variable
+    $url = $_SESSION['url'];
+
 if (isset($_SESSION['period_url']))//get period url from session variable
-    $period_url=$_SESSION['period_url'];
+    $period_url = $_SESSION['period_url'];
 
-if(isset($_SESSION['init_flag']))//check access flag status
+if (isset($_SESSION['init_flag']))//check access flag status
     $init_flag = $_SESSION['init_flag'];
 
-if(isset($_SESSION['Period_arr'])) //get array of periods in case of already processed 
+if (isset($_SESSION['Period_arr'])) //get array of periods in case of already processed 
     $Period_arr = $_SESSION['Period_arr'];
 
-if(isset($_SESSION['type'])) 
+if (isset($_SESSION['type']))
     $type = $_SESSION['type'];
-    
-if(isset($_SESSION['minBufferTime']))
+
+if (isset($_SESSION['minBufferTime']))
     $minBufferTime = $_SESSION['minBufferTime'];
-    
+
 
 $string_info = '<!doctype html> 
 <html lang="en">
@@ -150,20 +150,11 @@ $( "p" ).html( end);
 </body>
 </html>';
 
-
-
-
-function print_r2($val){ //Print output line by line (for testing)
-        echo '<pre>';
-        print_r($val);
-        echo  '</pre>';
+function print_r2($val) { //Print output line by line (for testing)
+    echo '<pre>';
+    print_r($val);
+    echo '</pre>';
 }
 
-
-
-
-process_mpd();// start processing mpd and get segments url
-
-
-	
+process_mpd(); // start processing mpd and get segments url
 ?>
