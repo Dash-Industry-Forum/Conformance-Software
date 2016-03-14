689fc2ca8db3224bf454e496406834b063ab7c11
Yi Li <yili.buaa@gmail.com>
2016-03-14 13:51:49 +0100

    Clean up the code and update the backend

--- webfe/assemble.php
+++ webfe/assemble.php
@@ -14,58 +14,52 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
 function Assemble ($path,$period,$sizearr)  // Assemble all segments into a single file
 {
-global $init_flag,$repno,$locate;
-if($init_flag) // if the segments contain intialization segment 
-$index = 0 ; // segments start at index 0
-else
-$index = 1; // segments start at index 1
+    global $init_flag, $repno, $locate;
+    if ($init_flag) // if the segments contain intialization segment 
+        $index = 0; // segments start at index 0
+    else
+        $index = 1; // segments start at index 1
 
-foreach($period as $unit)  
-$names[]=basename($unit); // create an array containing the names of all segments
-
-
-
-for ($i = 0;$i<sizeof($names);$i++){
-$fp1 = fopen($locate.'/'.$repno.".mp4", 'a+');  // Create container file to assemble all segments within it
-if(file_exists($path.$names[$i])){  // if file exist 
-
-$size=$sizearr[$i]; // Get the real size of the file (passed as inupt for function)
-$file2 = file_get_contents($path.$names[$i]); // Get the file contents
+    foreach ($period as $unit)
+        $names[] = basename($unit); // create an array containing the names of all segments
 
 
-fwrite($fp1,$file2); // bend it in the container file
-fclose($fp1);
-file_put_contents($locate.'/'.$repno.".txt",$index." ".$size."\n",FILE_APPEND); // add size to a text file to be passed to conformance software
-$index++; // iterate over all segments within the segments folder
 
-}
-}
+    for ($i = 0;$i<sizeof($names);$i++){
+        $fp1 = fopen($locate . '/' . $repno . ".mp4", 'a+');  // Create container file to assemble all segments within it
+        if (file_exists($path . $names[$i])) {  // if file exist 
+            $size = $sizearr[$i]; // Get the real size of the file (passed as inupt for function)
+            $file2 = file_get_contents($path . $names[$i]); // Get the file contents
 
+
+            fwrite($fp1, $file2); // bend it in the container file
+            fclose($fp1);
+            file_put_contents($locate . '/' . $repno . ".txt", $index . " " . $size . "\n", FILE_APPEND); // add size to a text file to be passed to conformance software
+            $index++; // iterate over all segments within the segments folder
+        }
+    }
 }
 
 /** This function remove all files and folders within the input folder (Used to delete old sessions to prevent accumulation of sessions on server**/
 function rrmdir($dir) {
-   if (is_dir($dir)) {
-     $objects = scandir($dir);
-     foreach ($objects as $object) {
-       if ($object != "." && $object != "..") {
-         if (filetype($dir."/".$object) == "dir")
-		 rrmdir($dir."/".$object);
-		 else {
-		 chmod($dir."/".$object,0777);
-		 unlink($dir."/".$object);
-		 }
-       }
-     }
-     reset($objects);
-     rmdir($dir);
-   }
- }
- 
-
+    if (is_dir($dir)) {
+        $objects = scandir($dir);
+        foreach ($objects as $object) {
+            if ($object != "." && $object != "..") {
+                if (filetype($dir."/".$object) == "dir")
+                    rrmdir($dir."/".$object);
+                else {
+                    chmod($dir."/".$object,0777);
+                    unlink($dir."/".$object);
+                }
+            }
+        }
+        reset($objects);
+        rmdir($dir);
+    }    
+}
 
 // Modification of standard PHP System() function to have system output from both the STDERR and STDOUT
 function syscall($command){
@@ -74,15 +68,15 @@ $result=0;
         while (!feof($proc)) $result .= fgets($proc, 1000);
         pclose($proc);
         return $result; 
-        }
     }
+}
 	
-	// Clean URL or Path in case of many slashes where created due to the lack of depth in BaseURL
-	function removeabunchofslashes($url){
-  $explode = explode('://',$url);
-  while(strpos($explode[1],'//'))
-    $explode[1] = str_replace('//','/',$explode[1]);
-  return implode('://',$explode);
+// Clean URL or Path in case of many slashes where created due to the lack of depth in BaseURL
+function removeabunchofslashes($url) {
+    $explode = explode('://', $url);
+    while (strpos($explode[1], '//'))
+        $explode[1] = str_replace('//', '/', $explode[1]);
+    return implode('://', $explode);
 }
 
 ?>
