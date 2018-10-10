6dc87eedc1f967b8436472ec6c33120700c85e0b
niteeshbhat <niteeshbhathm@gmail.com>
2018-10-10 13:21:40 +0200

    Upgrade insecure request- http to https mpd urls

--- webfe/conformancetest.php
+++ webfe/conformancetest.php
@@ -25,6 +25,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     </head>
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
@@ -813,7 +814,7 @@ function submit()
         });}
     else{  // Pass to server only, no JS response model.
            UrlExists(stringurl[0], function(urlStatus){
-                 //console.log(urlStatus);
+                 console.log(urlStatus);
                 if(urlStatus === 200 && stringurl[0]!=""){
                     $.post("process.php",{urlcode:JSON.stringify(stringurl),sessionid:JSON.stringify(SessionID),foldername: dirid});
                 }
@@ -1386,6 +1387,7 @@ function loadXMLDoc(dname)
     }
     xhttp.open("GET",dname,false);
     xhttp.send("");
+    console.log(xhttp.status);
     return xhttp.responseXML;
 }
 
